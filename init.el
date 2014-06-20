9a3fd5e493067b7b0d5cc0b89903bb8c99e7e0c9
Matt Dean <matt@trabian.com>
2014-06-19 22:37:56 -0500

    Initial commit
    Initial configuration taken from
    http://clojure-doc.org/articles/tutorials/emacs.html.
    
    Here we go!

---
+++ init.el
@@ -0,0 +1,13 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(defvar my-packages '(better-defaults
+		      clojure-mode
+		      clojure-test-mode
+		      cider))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
