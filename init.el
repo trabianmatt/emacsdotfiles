eb52e1f95210054667bf0ea467f692db7731122b
Matt Dean <matt@trabian.com>
2014-07-01 23:01:17 -0500

    More changes to config.

--- init.el
+++ init.el
@@ -19,17 +19,11 @@
 (require 'org)
 
 (require 'ob-tangle)
-(org-babel-load-file (expand-file-name "~/.emacs.d/mattdean.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/dev/first_tech/mobile/first-tech.org" "~/dev/vantage/banking/ob.org" "~/org/work.org" "~/org/personal.org" "~/.emacs.d/mattdean.org")))
- '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m org-habits))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ '(org-agenda-files (quote ("~/dev/first_tech/mobile/first-tech.org" "~/dev/vantage/banking/ob.org" "~/org/work.org" "~/org/personal.org" "~/.emacs.d/config.org"))))
+
