f42b2478b382740f4dd66d61117439c2e63c5bcd
Matt Dean <matt@trabian.com>
2014-11-25 12:46:35 -0500

    Update config

--- init.el
+++ init.el
@@ -1,31 +1,27 @@
-(package-initialize nil)
-;; Add any load-path customizations here if needed.
-;; For example: (add-to-list 'load-path "~/.emacs.d/forks/org-mode/lisp")
-(package-initialize t)
-
-;; org must be updated before starting any org-mode related functionality
-(require 'package)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-
-(defvar org-mode-packages '(org))
-
-(when (not package-archive-contents)
-   (package-refresh-contents))
-
-(dolist (p org-mode-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
-
-(require 'org)
-
-(require 'ob-tangle)
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/dev/sffcu/banking/sffcu.org" "~/dev/first_tech/mobile/first-tech.org" "~/dev/vantage/banking/ob.org" "~/org/work.org" "~/org/personal.org" "~/.emacs.d/config.org"))))
+ '(custom-safe-themes
+   (quote
+    ("3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+ '(js2-basic-offset 2 t)
+ '(magit-use-overlays nil)
+ '(org-agenda-files
+   (quote
+    ("~/dev/fcfcu/fcfcu.org" "~/dev/sffcu/banking/sffcu.org" "~/dev/first_tech/mobile/first-tech.org" "~/dev/vantage/banking/vantage.org" "~/org/work.org" "~/org/personal.org" "~/.emacs.d/config.org"))))
+
+(require 'cask "/usr/local/Cellar/cask/0.6.0/cask.el")
+(cask-initialize)
+(require 'pallet)
+
+(defvar org-mode-packages '(org))
+
+(require 'org)
+
+(require 'ob-tangle)
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
